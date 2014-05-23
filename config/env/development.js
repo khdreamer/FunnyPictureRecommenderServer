d27a648ac6dc0fba36da935710c08d51a04f8cb6
Kevin Huang <kevinhuangdreamer@gmail.com>
2014-05-23 12:40:25 +0800

    basic structure with jQuery

---
+++ config/env/development.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  db: 'mongodb://localhost/funny-picture-recommender-server-dev'
+};
---
+++ config/env/production.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  db: 'mongodb://localhost/funny-picture-recommender-server'
+};
+
